38a3b8e1e46ca5f524d26bad1230c4e8e9e2668e
chalkfung <chalkfung@gmail.com>
2022-10-20 16:09:20 +0800

    react boiler plate

---
+++ first-react/src/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const navbar = (
+    <nav>
+        <h1>Hello</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+/*
+ReactDOM.createRoot(document.getElementById("root")).render(navbar)*/
